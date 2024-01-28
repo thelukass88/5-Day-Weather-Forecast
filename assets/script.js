abaf4950b1e0ca65afb414504faa2fd74f5befdb
thelukass88 <l.holliday88@hotmail.com>
2024-01-28 20:27:27 +0000

    click event listener added

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
+var cityInput = document.querySelector("#city-search");
+var searchButton = document.querySelector(".search-btn");
 
+const getCoordinates = () => {
+    var cityName = cityInput.value.trim();
+    if(!cityName) return;
 
+    console.log(cityName)
+}
+
+searchButton.addEventListener("click", getCoordinates);
 
 // API Key
 const apiKey = "Y048bc7be255772a31199cccc135b1113"; 
