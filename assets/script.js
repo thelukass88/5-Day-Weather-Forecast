300f25edc8ce3de03f81acf49893b59c9df66da3
thelukass88 <l.holliday88@hotmail.com>
2024-01-28 21:35:19 +0000

    added fetch

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,21 @@
-var cityInput = document.querySelector("#city-search");
+var city = document.querySelector("#city-search");
 var searchButton = document.querySelector(".search-btn");
 
-const getCoordinates = () => {
-    var cityName = cityInput.value.trim();
-    if(!cityName) return;
+// API Key
+const apiKey = "048bc7be255772a31199cccc135b1113"; 
 
-    console.log(cityName)
+const getCoordinates = () => {
+    var cityName = city.value.trim();
+    if(!cityName) return;
+    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;;
+
+    fetch(API_URL).then(res => res.json()).then(data => {
+        console.log(data)
+    }).catch(() => {
+        alert("An error occured while fetching the coordinates!");
+    })
+
 }
 
 searchButton.addEventListener("click", getCoordinates);
 
-// API Key
-const apiKey = "Y048bc7be255772a31199cccc135b1113"; 
