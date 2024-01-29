2e94c346e58ccb715d000d007761b484cedb5029
thelukass88 <l.holliday88@hotmail.com>
2024-01-29 07:33:14 +0000

    weather cards now call through array from API data

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,16 @@ var city = document.querySelector("#city-search");
 var searchButton = document.querySelector(".search-btn");
 
 // API Key
-const apiKey = "048bc7be255772a31199cccc135b1113"; 
+const apiKey = ""; 
 
 const createWeatherCard = (weatherItem) => {
-    return '';
+    return `<li class="card">
+    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+</li>`;
 }
 
 const getWeatherData = (cityName, lat, lon) => {
